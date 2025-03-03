6e8c2fec388437c20d8c396255cf830c9ba819e0
dewmindi <udewmindi.ud@gmail.com>
2025-03-04 03:34:48 +0530

    updated

--- src/components/evaluation/EvaluationCreator.tsx
+++ src/components/evaluation/EvaluationCreator.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Header } from '../Header'
-import { ArrowBigLeft, ArrowLeftCircleIcon, Upload } from 'lucide-react'
+import { ArrowBigLeft,Upload } from 'lucide-react'
 
 interface Exam {
   id: string
